3f5d2361e9e2fe6491c5e34db8cee026a08992e0
Himanshu4368 <114290034+Himanshu4368@users.noreply.github.com>
2022-09-24 17:44:51 +0530

    Add files via upload

---
+++ Class four.java
@@ -0,0 +1,12 @@
+class three{
+    public static void main(String arg[]){
+       int a =3;
+       int b =5;
+       int k=a;
+       a=b;
+        b=k;
+        System.out.println("value of a is ="+a);
+        System.out.println("value of b="+b);
+     
+    }
+}
